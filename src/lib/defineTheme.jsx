80aa37676b10a5c686810ddef62bab4421455e75
Nimely111 <samuelehnimely@gmail.com>
2025-06-23 21:08:13 +0000

    allow vite to ignore the dynamic import

--- src/lib/defineTheme.jsx
+++ src/lib/defineTheme.jsx
@@ -55,7 +55,9 @@ const defineTheme = (theme) => {
   return new Promise((res) => {
     Promise.all([
       loader.init(),
-      import(`monaco-themes/themes/${monacoThemes[theme]}.json`),
+      import(
+        /* @vite-ignore */ `monaco-themes/themes/${monacoThemes[theme]}.json`
+      ),
     ]).then(([monaco, themeData]) => {
       monaco.editor.defineTheme(theme, themeData);
       res();
